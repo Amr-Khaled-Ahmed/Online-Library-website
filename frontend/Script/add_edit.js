66c6de124dbd5d80df8a1fd894746d246db0da90
Ahmed <as3992054@gmail.com>
2025-04-17 11:56:12 +0200

    Upload book cover by click or drag and drop

--- frontend/Script/add_edit.js
+++ frontend/Script/add_edit.js
@@ -7,14 +7,111 @@ document.addEventListener('DOMContentLoaded', function() {
 
     document.querySelector('form').addEventListener('submit', () => {
         sessionStorage.setItem('showMessage', 'true');
+
+        // Save book info
+        const bookData = {
+            title: document.getElementById('title').value,
+            author: document.getElementById('author').value,
+            coverPath: document.querySelector('.book-cover').src,
+            genre: document.getElementById('genre').value,
+            format: document.getElementById('format').value,
+            yearPub: document.getElementById('pub-time').value,
+            availability: document.getElementById('availability').value,
+            borrowNum: (window.sessionStorage.getItem('edit') === 'true') ? bookParams.get('borrowNum') : '0',
+            maxDuration: document.getElementById('mx-brw-dur').value,
+            lateFees: document.getElementById('late-fee').value
+        };
+
+
+        // Save book to database
+
     });
+
     document.getElementById('save').addEventListener('click',() => saveBook(book));
+
+    let imgHolder = document.querySelector('.img-holder');
+    let imgInput = document.getElementById('img-input');
+    let img = document.querySelector('.book-cover');
+    let errorNotification = document.querySelector('.error');
+
+    function loadImg(files) {
+        const file = files[0];
+        if(!file.type.match('image.*')) {
+            errorNotification.classList.remove('hide');
+            errorNotification.style.animation = 'notification 1.7s ease-in-out 2s backwards';
+            errorNotification.addEventListener('animationend',() => {
+                errorNotification.classList.add('hide');
+                errorNotification.style.animation = '';
+            });
+            input.value = '';
+            return;
+        }
+
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+
+        reader.onload = function(e) {
+            img.src = e.target.result;
+            document.querySelector('.upload').style.display = 'none';
+            img.classList.remove('hide');
+        }
+    }
+    imgHolder.addEventListener('click',() => imgInput.click());
+    imgInput.addEventListener('change', () => {
+        if(imgInput.files.length) {
+            loadImg(imgInput.files);
+        }
+    });
+
+    ['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
+        imgHolder.addEventListener(eventName, preventDefaults, false);
+        document.body.addEventListener(eventName, preventDefaults, false);
+    });
+
+    function preventDefaults(e) {
+        e.preventDefault();
+        e.stopPropagation();
+    }
+
+    imgHolder.addEventListener('drop', (e) => {
+        const dt = e.dataTransfer;
+        const files = dt.files;
+    
+        if (files.length) {
+            loadImg(files);
+        }
+    });
+
+    function willUpdate() {
+        document.querySelector('.upload').style.display = 'flex';
+        imgHolder.style.border = '3px dashed #9ea1a4';
+        if(img.naturalWidth > 0 && img.style.display !== 'none') {
+            img.style.opacity = 0.3;
+        }
+    }
+
+    function notUpdated() {
+        if(img.naturalWidth > 0 && img.style.display !== 'none') {
+            document.querySelector('.upload').style.display = 'none';
+            imgHolder.style.border = 'none';
+            img.style.opacity = 1;
+        }
+    }
+
+    imgHolder.addEventListener('mouseover',() => willUpdate());
+    imgHolder.addEventListener('dragover',() => willUpdate());
+
+    imgHolder.addEventListener('mouseout',() => notUpdated());
+    imgHolder.addEventListener('dragleave',() => notUpdated());
 
     window.addEventListener('load',() => {
         if(window.sessionStorage.getItem('edit') === 'true') {
             const bookParams = new URLSearchParams(window.location.search);
-
-            document.querySelector('.book-cover').src = bookParams.get('coverPath');
+            
+            document.querySelector('.upload').style.display = 'none';
+            let img = document.querySelector('.book-cover');
+            img.classList.remove('hide');
+            img.src = bookParams.get('coverPath');
             document.getElementById('title').value = bookParams.get('title');
             document.getElementById('author').value = bookParams.get('author');
             document.getElementById('genre').value = bookParams.get('genre');
@@ -24,23 +121,8 @@ document.addEventListener('DOMContentLoaded', function() {
             document.getElementById('late-fee').value = bookParams.get('lateFees');
             document.getElementById('mx-brw-dur').value = bookParams.get('maxDuration');
         }
+        else {
+            document.querySelector('.upload').style.display = 'flex';
+        }
     });
 });
-
-// Save Book to database
-function saveBook(book) {
-    const bookData = {
-        title: document.getElementById('title').value,
-        author: document.getElementById('author').value,
-        coverPath: document.querySelector('.book-cover').src,
-        genre: document.getElementById('genre').value,
-        format: document.getElementById('format').value,
-        yearPub: document.getElementById('pub-time').value,
-        availability: document.getElementById('availability').value,
-        borrowNum: (window.sessionStorage.getItem('edit') === 'true') ? bookParams.get('borrowNum') : '0',
-        maxDuration: document.getElementById('mx-brw-dur').value,
-        lateFees: document.getElementById('late-fee').value
-    };
-
-    
-}
