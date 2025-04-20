72ce8ebff6ec9a376985026cf259de5887d0257c
mohamedahmed2005 <162487393+mohamedahmed2005@users.noreply.github.com>
2025-04-20 11:54:25 +0200

    function of star button

--- frontend/Script/borrowed.js
+++ frontend/Script/borrowed.js
@@ -130,3 +130,10 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 });
+
+document.querySelectorAll('.star-button').forEach(button => {
+    button.addEventListener('click', (event) => {
+        event.stopPropagation();
+        button.classList.toggle('active');
+    });
+});
